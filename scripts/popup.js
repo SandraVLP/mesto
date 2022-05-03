33bdd8b39c0cfdc57633127652b9d697575255bc
SandraVLP <aleksanvp@yandex.ru>
2022-05-03 12:29:30 +0300

    ver 0305

---
+++ scripts/popup.js
@@ -0,0 +1,39 @@
+const popupOpnBtn = document.querySelector(".button__add");
+const popup = document.querySelector(".popup");
+const popupClsBtn = document.querySelector(".popup__close");
+const title = document.querySelector(".profile__title");
+const subtitle = document.querySelector(".profile__subtitle");
+const popupTitle = document.querySelector(".popup__profile-title");
+const popupSubtitle = document.querySelector(".popup__profile-subtitle");
+const popupSaveBtn = document.querySelector(".popup__save");
+
+function openPopup () {
+
+    popup.classList.add("popup_active");
+    popupTitle.value = title.textContent;
+    popupSubtitle.value = subtitle.textContent;
+
+}
+
+function closePopup () {
+    popup.classList.remove("popup_active");
+}
+
+function submitPopup (event) {
+    event.preventDefault();
+    title.textContent = popupTitle.value;
+    subtitle.textContent = popupSubtitle.value;
+    closePopup ();
+    
+}
+
+popupOpnBtn.addEventListener("click", openPopup);
+
+
+popupClsBtn.addEventListener("click", closePopup);
+
+popupSaveBtn.addEventListener("click", submitPopup);
+
+
+
+
