8683f164e3db6539514881286f247faa87147207
SandraVLP <aleksanvp@yandex.ru>
2022-07-17 20:41:25 +0300

    4

--- scripts/popup.js
+++ scripts/popup.js
@@ -54,6 +54,8 @@ function  closePopupEditProfile () {
 };
 
 function closeAddCardPopup () {
+    newCardTitleInput.value = "";
+    newCardLinkInput.value = "";
     closePopup(popupAddCard);
 };
 
